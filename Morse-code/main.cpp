16e72c4001dbb31d8edb672a5d36776a6b63fce7
Gerald Shimo <shimogerald450@gmail.com>
2025-01-25 01:22:08 -0600

    first commit

--- Morse-code/main.cpp
+++ Morse-code/main.cpp
@@ -1,14 +1,60 @@
-//
-//  main.cpp
-//  Morse-code
-//
-//  Created by Gerald Shimo on 1/24/25.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// Function prototypes
+string getUserString();
+string getMorseCode(const string &input);
+void printMorse(const string &morseCode);
+
+int main() {
+    
+    string userInput = getUserString();
+
+ 
+    string morseCode = getMorseCode(userInput);
+
+  
+    printMorse(morseCode);
+
     return 0;
+}
+
+
+string getUserString() {
+    string input;
+    cout << "Enter string to be converted: ";
+    getline(cin, input);
+    return input;
+}
+
+
+string getMorseCode(const string &input) {
+    string letter[] = {" " , ",", ".", "?", "0","1", "2", "3","4","5", "6", "7","8",
+                    "9","A" ,"B","C","D","E","F", "G","H","I","J","K","L","M","N"
+                    ,"O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+
+    string morse[] = {"space","--..--",".-.-.-","..--..","-----",".----","..---",
+                        "...--","....-",".....","-....","--...","---..","----.",
+                        ".-","-...","-.-.","-..",".","..-.","--.","....","..",
+                        ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...",
+                        "-","..-","...-",".--","-..-","-.--","--.."};
+
+    string output;
+    for (size_t i = 0; i < input.length(); i++) {
+        char let = toupper(input[i]);
+
+        for (size_t y = 0; y < sizeof(letter) / sizeof(letter[0]); y++) {
+            if (let == letter[y][0]) {
+                output += morse[y];
+                break;
+            }
+        }
+    }
+
+    return output;
+}
+
+void printMorse(const string &morseCode) {
+    cout << "The Morse code equivalent is: " << morseCode << endl;
 }
